f984933169726e753843bcd3f9968da467dadb94
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-07-22 04:58:35 -0300

    Accordion to Class

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,22 +1,29 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    // eslint-disable-next-line comma-dangle
-    '[data-anime="accordion"] dt'
-  );
-
-  const activeClass = 'ativo';
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(listaAccordion) {
+    this.accordionList = document.querySelectorAll(listaAccordion);
+    this.activeClass = 'ativo';
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+    return this;
+  }
 
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => {
+        this.toggleAccordion(item);
+      });
     });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+
+    return this;
   }
 }
