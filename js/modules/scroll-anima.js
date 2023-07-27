147b7ad4e29a95d88f5ff95b2c783ac654a26d6c
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-07-27 05:45:08 -0300

    redo scroll animation

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,43 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  // verifica a distancia em cada objeto em relacao ao scroll do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Pega a distancia de cada item em relacao ao topo
+  // do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add('ativo');
+      }
+    });
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o event de scroll caso necessario
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
+}
